de0989f2e723fc57c7555ecfef9292deaa19f119
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2022-10-28 14:20:17 -0300

    Fix: set alt to all images

--- js/index.js
+++ js/index.js
@@ -4,10 +4,12 @@ function makeNews() {
     {
       description: "O grupo Katie está organizando o evento de comemoração de aniversário!Se inscrevam para o evento ainda hoje, terá eventos presenciais e online.",
       logo_src: "../images/katie_logo.png",
+      alt: "Logo Katie",
     },
     {
       description: "Grupo formado com o intuito de levar informações sobre cibersegurança ao bloco e montar grupos de estudo intensivo na área, perfeito para quem quer entender InfoSec.",
       logo_src: "../images/acha_logo.png",
+      alt: "Logo Acha",
     },
   ]
   
@@ -15,7 +17,7 @@ function makeNews() {
     return acc + `
     <article class="w-fit px-20 py-7 bg-white flex gap-[140px] rounded-[40px] border-4 border-[#4245DB] items-center">
       <p id="description" class="w-[400px] text-2xl">${item.description}</p>
-      <img src="${item.logo_src}" alt="Logo Katie" class="rounded-xl">
+      <img src="${item.logo_src}" alt="${item.alt}" class="rounded-xl">
     </article>`
   }, '');
 
